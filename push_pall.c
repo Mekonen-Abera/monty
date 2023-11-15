57c55a0ab9d31c50940c7d220f072f4dc288d7db
Mekonen-Abera <makoninabera@gmail.com>
2023-11-15 05:58:55 -0800

    C - Stacks, Queues - LIFO, FIFO

--- push_pall.c
+++ push_pall.c
@@ -33,3 +33,37 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 }
 
+/**
+ * pall - Prints all the value in stack
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
+ * Done by: Mekonen and Gebrekidan
+ * Return: Nothing
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+
+	stack_t *temp = NULL;
+
+
+	if (*stack == NULL)
+	{
+		return;
+	}
+	if (*stack == NULL && line_number != 1)
+	{
+		free_dlistint(*stack);
+		free_globalvars();
+		exit(EXIT_SUCCESS);
+	}
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+	while (temp->prev != NULL)
+	{
+		printf("%d", temp->n);
+		temp = temp->prev;
+		printf("\n");
+	}
+	printf("%d\n", temp->n);
+}
