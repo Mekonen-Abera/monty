28600b6bf560f4ce3e3747c3cced28738d3b750c
Mekonen-Abera <makoninabera@gmail.com>
2023-11-16 00:34:15 -0800

    Stack and Queue

--- stack.c
+++ stack.c
@@ -1,37 +1,56 @@
 #include "monty.h"
 
 /**
- * free_stack - freedoubly linked list
- * @head: first node of stack
- * Return: none
+ * create_node_stack - create a node stack.
+ * @stack: A pointer to top and bottom stack.
+ * Done by: Mekonen Abera & Gebrekidan Alemayehu
+ * Return: (EXIT SUCCESS)
  */
-
-void free_stack(stack_t *head)
+int create_node_stack(stack_t **stack)
 {
-	stack_t *temp;
+	stack_t *p;
 
-	temp = head;
-	while (head)
+	p = malloc(sizeof(stack_t));
+	if (p == NULL)
+		stderr_malloc();
+	p->n = STACK;
+	p->prev = NULL;
+	p->next = NULL;
+
+	*stack = p;
+
+	return (EXIT_SUCCESS);
+}
+/**
+ * free_node_stack - Frees a stack_t stack.
+ * @stack: A pointer to top and bottom stack.
+ * Done by: Mekonen Abera & Gebrekidan Alemayehu
+ * Return: Nothing
+ */
+void free_node_stack(stack_t **stack)
+{
+	stack_t *temp_node = *stack;
+
+	while (*stack)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp_node = (*stack)->next;
+		free(*stack);
+		*stack = temp_node;
 	}
 }
-
 /**
- * f_stack - free stack
- * @head: first node(unused)
- * @counter: line counter (unused)
- *
- * Return: none
-*/
-
-void f_stack(stack_t **head, unsigned int counter)
+ * check_opcode - checks if STACK or QUEUE.
+ * @stack: A pointer to top and bottom stack.
+ * Done by: Mekonen Abera & Gebrekidan Alemayehu
+ * Return: The value of Stack or Queue
+ */
+int check_opcode(stack_t *stack)
 {
-	(void)head;
-	(void)counter;
+	int ret_val = 2;
 
-	bus.lifi = 0;
+	if (stack->n == STACK)
+		return (STACK);
+	else if (stack->n == QUEUE)
+		return (QUEUE);
+	return (ret_val);
 }
-
