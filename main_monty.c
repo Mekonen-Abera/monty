414dfc8b546337062c15c950cdda34c6aa03652e
Mekonen-Abera <makoninabera@gmail.com>
2023-11-16 22:27:36 -0800

    updated MAin

--- main_monty.c
+++ main_monty.c
@@ -9,30 +9,39 @@ globales_t globalvar = {NULL, NULL, NULL};
 
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
 
+/**
+ * main - entry point for the CLI program
+ * @argc: count of arguments passed to the program
+ * @argv: pointer to an array of characters
+ * Done by: Mekonen Abera & Gebrekidan Alemayehu
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+
 int main(int argc, char **argv)
 {
-    char *token = NULL;
-    size_t line_buf_size = 0;
-    int line_number = 0, flag = 0, flag2 = 0;
-    ssize_t line_size;
-    stack_t *stack = NULL;
+	char *token = NULL;
+	size_t line_buf_size = 0;
+	int line_number = 0, flag = 0, flag2 = 0;
+	ssize_t line_size;
+	stack_t *stack = NULL;
 
     if (argc != 2)
-        stderr_usage();
+	    stderr_usage();
     globalvar.fd = fopen(argv[1], "r");
     if (globalvar.fd == NULL)
-        stderr_fopen(argv[1]);
+	    stderr_fopen(argv[1]);
     line_size = my_getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
     if (globalvar.line_buf[0] == '#')
-        line_size = my_getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
+	    line_size = my_getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
     while (line_size >= 0)
     {
-        flag = 0;
-        flag2 = 0;
-        line_number++;
-        token = strtok(globalvar.line_buf, DELIM);
-        globalvar.token2 = strtok(NULL, DELIM);
-        if (token == NULL)
+	    flag = 0;
+	    flag2 = 0;
+	    line_number++;
+	    token = strtok(globalvar.line_buf, DELIM);
+	    globalvar.token2 = strtok(NULL, DELIM);
+	
+	if (token == NULL)
         {
             flag2 = 1;
             nop(&stack, line_number);
